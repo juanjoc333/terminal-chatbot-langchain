ec292b1d4aa15a3c6686551a9cbd1ee1c1699f64
Juan Cortes <jcortes@fullstacklabs.com>
2023-10-25 19:45:17 -0600

    Implementing basic chat chain

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    input_variables="content",
+    messages=[
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt
+)
+
 while True:
     content = input(">> ")
 
-    print(f"Your input was: {content}")
+    result = chain({"content": content})
+
+    print(result["text"])
